214a22aa6c918af105d663e321c7b70ae6845ca6
jquevedo82 <ing.cheo@gmail.com>
2022-10-02 18:09:51 -0300

    feat: aspect-radio [2/1] modify

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,9 +13,16 @@
     <div class="container py-12">
         <div class="grid grid-cols-3 gap-6">
             <div class="col-span-2">
-                <iframe class="w-full aspect-video " src="https://www.youtube.com/embed/r1ySsbIA7EU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe class="w-full aspect-video " src="https://www.youtube.com/embed/r1ySsbIA7EU"
+                    title="YouTube video player" frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen></iframe>
+                <div class="bg-red-400 aspect-square"></div>
+                <div class="bg-green-400 aspect-[2/1]"></div>
             </div>
-            <div class="col-span-1 bg-blue-500"></div>
+            <div class="col-span-1 bg-blue-500">
+
+            </div>
         </div>
     </div>
 </body>

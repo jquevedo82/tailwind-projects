b5164e25e2e13bf42899b5391ce95ca3476a1003
jquevedo82 <ing.cheo@gmail.com>
2022-10-02 18:32:31 -0300

    feat: text break after or before

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,13 +11,36 @@
 
 <body>
     <div class="container py-12">
-        <div class="columns-4 gap-4">
-            <img src="https://cdn.pixabay.com/photo/2022/08/21/21/24/colours-7402147_960_720.jpg" alt="">
-            <img src="https://cdn.pixabay.com/photo/2022/08/17/08/16/bird-7391868_960_720.jpg" alt="">
-            <img src="https://cdn.pixabay.com/photo/2022/09/06/10/57/love-7436367_960_720.jpg" alt="">
-            <img src="https://cdn.pixabay.com/photo/2022/09/20/20/22/blueberries-7468718_960_720.jpg" alt="">
-            <img src="https://cdn.pixabay.com/photo/2022/09/16/09/22/fish-7458229_960_720.png" alt="">
-            <img src="https://cdn.pixabay.com/photo/2022/09/19/23/28/church-7466633_960_720.jpg" alt="">
+        <div class="columns-3">
+            <p class="bg-red-200">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ornare magna ut lacus lacinia, vel porta
+                lectus lobortis. Suspendisse volutpat purus nec mauris blandit, vel euismod turpis porta. In
+                pellentesque mauris in congue congue. Nulla in scelerisque lacus. Nam et velit ultrices, sagittis neque
+                ac, ornare eros. Nunc sed lobortis sapien. Nulla ut suscipit ex, nec volutpat ante. Cras et velit dolor.
+                Nam auctor egestas tellus, eget semper arcu. Quisque aliquet lacinia orci id blandit.</p>
+            <p class="bg-blue-500 break-after-column">Donec eget libero et nibh scelerisque lobortis. Mauris viverra dui ut arcu porta, vitae blandit ex
+                tempor. Sed ut pretium magna. Quisque semper porta ipsum, ac convallis ex ultricies ac. Cras velit odio,
+                commodo vel tincidunt quis, condimentum sit amet odio. Phasellus ultricies quam nec ligula ornare, at
+                varius nibh porttitor. Suspendisse vel elit vel tellus fringilla scelerisque. Interdum et malesuada
+                fames ac ante ipsum primis in faucibus. Mauris a feugiat dolor, convallis vestibulum turpis. Fusce
+                varius ante vitae quam tristique ullamcorper. Donec eget erat at risus pulvinar scelerisque. Praesent
+                mollis sollicitudin justo, sit amet varius arcu. In hac habitasse platea dictumst. Integer venenatis a
+                mi quis rutrum. Nam viverra nibh non ultrices hendrerit. Nulla ut libero eu mauris cursus vehicula.</p>
+            <p class="bg-green-200">In suscipit sem sit amet vulputate mollis. Donec vel luctus dolor. Nam in sagittis libero, imperdiet
+                commodo nulla. Proin dignissim maximus felis, vitae congue est consectetur ut. Nullam quis suscipit
+                tellus, quis egestas ipsum. Donec a nulla mattis, sollicitudin lectus et, lobortis enim. Pellentesque
+                metus neque, placerat id tortor at, pulvinar interdum sem. Aenean sem neque, egestas eu risus a,
+                accumsan aliquam metus. Fusce at tortor ac nisl congue cursus. Nulla quis sollicitudin sem, non
+                scelerisque nulla.</p>
+            <p class="bg-indigo-200 break-before-column">Phasellus consequat ultrices justo lobortis posuere. Proin facilisis dolor turpis, quis placerat nisl
+                vulputate quis. Fusce sed lacus enim. Phasellus ut ultricies ipsum. Suspendisse eu magna augue. Vivamus
+                urna tellus, suscipit pulvinar blandit id, porta eget orci. Donec ut quam ac ligula maximus dictum
+                sollicitudin vitae diam. Integer sed imperdiet elit, quis dapibus lacus. Morbi efficitur posuere lorem
+                vitae ultricies. Donec sollicitudin nisi ac libero fermentum tincidunt.</p>
+            <p class="bg-yellow-200">Pellentesque eu ante in quam sodales congue. Cras at gravida leo. Donec efficitur pretium sem venenatis
+                faucibus. Aenean finibus ipsum vitae feugiat elementum. In pellentesque, velit non bibendum porttitor,
+                est massa ultricies sem, non dignissim purus turpis sit amet enim. Pellentesque egestas in risus et
+                mattis. Fusce est ex, fringilla ac accumsan non, sollicitudin a nisl. Proin lobortis ipsum at dui
+                ornare, sed dignissim velit iaculis. Sed elementum quam nulla, sed sagittis lorem vestibulum ut.</p>
         </div>
     </div>
 </body>

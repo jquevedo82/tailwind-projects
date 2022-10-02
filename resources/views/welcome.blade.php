761df5979b887d802e9e410bc8f455b0be174574
jquevedo82 <ing.cheo@gmail.com>
2022-10-02 18:24:43 -0300

    feat: image columns gap-4

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,18 +11,13 @@
 
 <body>
     <div class="container py-12">
-        <div class="grid grid-cols-3 gap-6">
-            <div class="col-span-2">
-                <iframe class="w-full aspect-video " src="https://www.youtube.com/embed/r1ySsbIA7EU"
-                    title="YouTube video player" frameborder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    allowfullscreen></iframe>
-                <div class="bg-red-400 aspect-square"></div>
-                <div class="bg-green-400 aspect-[2/1]"></div>
-            </div>
-            <div class="col-span-1 bg-blue-500">
-
-            </div>
+        <div class="columns-4 gap-4">
+            <img src="https://cdn.pixabay.com/photo/2022/08/21/21/24/colours-7402147_960_720.jpg" alt="">
+            <img src="https://cdn.pixabay.com/photo/2022/08/17/08/16/bird-7391868_960_720.jpg" alt="">
+            <img src="https://cdn.pixabay.com/photo/2022/09/06/10/57/love-7436367_960_720.jpg" alt="">
+            <img src="https://cdn.pixabay.com/photo/2022/09/20/20/22/blueberries-7468718_960_720.jpg" alt="">
+            <img src="https://cdn.pixabay.com/photo/2022/09/16/09/22/fish-7458229_960_720.png" alt="">
+            <img src="https://cdn.pixabay.com/photo/2022/09/19/23/28/church-7466633_960_720.jpg" alt="">
         </div>
     </div>
 </body>
